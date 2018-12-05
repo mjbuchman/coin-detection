b9ab355c6ae541508d5bb0762ec504393db6a114
deryk96 <45543642+deryk96@users.noreply.github.com>
2018-12-05 10:06:41 -0600

    Add files via upload
    Minor changes and added sections to the main file

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-% Load image, compress, and convert to grayscale
+%% ---------------------------- Load images -------------------------------
 img = imread('test-images\42c.jpg');
 img = imresize(img, 0.5);
 grayimg = rgb2gray(img);
@@ -9,7 +9,7 @@ I = imtophat(grayimg,strel('disk',200));
 bw = imbinarize(I);
 bw = bwareaopen(bw, 50);
 
-% Isolate pennies from rest of coins
+%% ---------------------------- Find Pennies-------------------------------
 hsv_image = rgb2hsv(img);
 BW_HSV = im2bw(hsv_image, 0.36);
 penny_mask = imfill(BW_HSV, 'holes');
@@ -20,17 +20,20 @@ imgPenny(:,:,1) = double(imgPenny(:,:,1)).*penny_mask;
 imgPenny(:,:,2) = double(imgPenny(:,:,2)).*penny_mask;
 imgPenny(:,:,3) = double(imgPenny(:,:,3)).*penny_mask;
 
-% Find centers and radii of all pennies
 [pennyCenters, pennyRadii] = findCircles(imgPenny)
-
-% Find and visualize all coins
-imshow(img);
-hold on;
-[allCenters, allRadii] = findCircles(bw)
 
 % Check for pennies and return the average radius of all pennies in image
 averagePennyR = findPennyRadius(pennyCenters, pennyRadii)
 
+%% ---------------------------- Detect Other Coins ------------------------
+% Show original image
+imshow(img);
+hold on;
+
+% Detect (and visualize) other coins
+[allCenters, allRadii] = findCircles(bw)
+
+%% ---------------------------- Count All Coins ---------------------------
 % Compare all radii to the penny and count total change
 % Also calls method to visually overlay value on each coin
 total = 0;
