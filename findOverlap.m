8086a1808e912319632d04fa38418eff7e370a3b
dkim355 <34180010+dkim355@users.noreply.github.com>
2018-12-09 13:56:41 -0600

    Update findOverlap.m

--- findOverlap.m
+++ findOverlap.m
@@ -4,7 +4,7 @@ bboxA = [allCenters(i,1)-allRadii(i), allCenters(i,2)-allRadii(i), allRadii(i)*2
     for j = i+1 : size(allCenters) - 1
         bboxB = [allCenters(j+1,1)-allRadii(j+1), allCenters(j+1,2)-allRadii(j+1), allRadii(j+1)*2, allRadii(j+1)*2];
         overlapRatio = bboxOverlapRatio(bboxA, bboxB);
-        if overlapRatio > 0.2
+        if overlapRatio > 0.3
         flag_error = 1;
         return;
         end
